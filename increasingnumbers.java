f15907cfde203ee8418e398484252096b7bb5cde
Gowthamjittu <gowthamjittu2022@gmail.com>
2025-02-17 23:26:54 +0530

    create a new file

--- increasingnumbers.java
+++ increasingnumbers.java
@@ -1,16 +1,14 @@
 public class increasingnumbers {
-    public static void main(String args[]){
-        int n=4;
-        int count=1;
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<i;j++){
+    public static void main (String []args){
+        int n = 4;
+        for (int i = 0; i<=n ; i++){
+            for (int j = 0 ; j<=i ; j++){
+
+                System.out.print(i );
+            
                 System.out.print(" ");
             }
-            for(int j=0;j<=n-i;j++){
-                System.out.print("*");
-                count++;
-            }
-        System.out.println();
+            System.out.println();
         }
-}
+    }
 }
--- trianglepattern.java
+++ trianglepattern.java
@@ -3,10 +3,7 @@ public class trianglepattern {
         int n = 4;
         for (int i = 1 ; i <= n ; i++){
             for (int j = 1 ; j <= i ; j++){
-                System.out.print("* ");
-                System.out.print(" ");
-
-
+                System.out.print("*  ");
             }
             System.out.println();
         }
