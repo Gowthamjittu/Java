b230f8a55706da60abcead8e1d8d6650dbfb58de
Gowthamjittu <gowthamjittu2022@gmail.com>
2025-02-19 10:28:47 +0530

    reverse array

--- array.java
+++ array.java
@@ -1,6 +1,9 @@
 public class array{
     public static void main(String []args){
-        int[] a = {54, 95, 35, 44, 85, 02};
-        System.out.println(a[3]);
+        int[] a ={54, 95, 35, 44, 85, 02};
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            System.out.print(a [i]+" ");
+        }
     }
 }
