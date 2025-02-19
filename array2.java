2bbaa9b4588f56a44e0ec20f9bd3e6e40d5536eb
Gowthamjittu <gowthamjittu2022@gmail.com>
2025-02-19 15:20:37 +0530

    array 2

--- array2.java
+++ array2.java
@@ -3,11 +3,11 @@ public class array2 {
         int[] a = {5, 7, 2, 11};
         int temp = a[a.length-1];
         for(int i = a.length-2 ; i >= 0 ; i-- ){
-            a[i] = a[i+1];
+            a[i+1] = a[i];
         }
         a[0] = temp;
         for (int i = 0 ; i < a.length; i++){
-            System.out.print(a[i]);
+            System.out.print(a[i]  + ",");
 
         }
     }
