f69e0ab9376fef6ce5ed4fa26ffa3b38f6fde393
Gowthamjittu <gowthamjittu2022@gmail.com>
2025-02-19 11:45:37 +0530

    reverse array

--- reversearray.java
+++ reversearray.java
@@ -1,13 +1,14 @@
-public class reversearray {
+public class reversearray{
     public static void main (String []args){
-        int [] a = {2, 5, 7, 9, 20};
-        for (int i = 0 ; i < a.length / 2 ; i++){
+        int[] a = {3, 4, 7, 2, 9};
+        for (int i  = 0 ; i < a.length / 2 ; i++){
             int temp = a[i];
-            a[i]=a[a.length-1-i];
-            a[a.length-1-i]= temp;
+            a[i] = a[a.length-1-i];
+            a[a.length-1-i] = temp;
+
         }
-        for (int i = 0 ; i < a.length; i++){
-            System.out.print(a[i]+ " ");
+        for (int i = 0 ; i < a.length ; i++){
+            System.out.print(a[i]+" ");
         }
     }
 }
