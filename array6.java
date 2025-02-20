4b57f38436d5907a782f6968f8f37a3e30f6ad9e
Gowthamjittu <gowthamjittu2022@gmail.com>
2025-02-20 14:34:59 +0530

    adding number in array

--- array6.java
+++ array6.java
@@ -2,17 +2,16 @@ import java.util.Arrays;
 
 public class array6 {
     public static void main ( String [] aargs){
-        int a[] ={5, 6, 12, 7, 8, 1, 4, -3};
+        int a[] ={5, 6, 12, 7, 8, 1, 4, -3, 9};
         for (int i = 0 ; i < a.length ; i++){
             for ( int j = i+1 ; j < a.length ; j++){
                 if (a[i] + a[j] == 9){
                     System.out.print("(" + a[i]+","+a[j] + ")" );
-
                 }
+                }
+
             }
         }
+    }
         
 
-    }
-    
-}
