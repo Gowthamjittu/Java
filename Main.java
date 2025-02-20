e9bc7351ec6a6cd74a319289a98cf9a00a0b5a12
Gowthamjittu <gowthamjittu2022@gmail.com>
2025-02-20 09:55:52 +0530

    finding odd even by array

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main{
     public static void main (String []args){
       int [] a = {1, 5, 6, 9, 3, 7};
+      int temp = a[a.length-1];
+      for (int i = a.length-1 ; i >= 0 ; i--){
+        a[i+1] = a[i];
+
+      }
+      for (int i = 0 ; i < a.length ; i++){
+        System.out.print(a[i] + ",");
+      }
+
     }
 
 }
---
+++ array5.java
@@ -0,0 +1,12 @@
+public class array5 {
+    public static void main (String [] args){
+        int[] a = {5, 3, 2, 5, 1, 4, 3};
+        for (int i = 0 ; i < a.length ; i++){
+            for (int j = i+1 ; j <a.length ; j++){
+                if ( i == j);
+                System.out.print("same number");
+            }
+        }
+    }
+    
+}
